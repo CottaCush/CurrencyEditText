471d1ad626020baf47690c5b966610197c5d2d76
EfeDaniel <EfeEjemudaro@gmail.com>
2021-07-02 16:48:44 +0100

    Feature: Make make maximumNumberOfDecimalPlaces configurable

---
+++ library/src/main/java/com/cottacush/android/currencyedittext/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.cottacush.android.currencyedittext.utils
+
+object Constants {
+
+    object Messages {
+        const val MAX_DECIMAL_PLACES_ERROR = "Maximum number of Decimal Places must be a positive integer"
+    }
+
+}
